79bc6826bd5f916a69e184f6ee65dd087fbfb07d
Aniket Prajapati <prajapati.ani306@gmail.com>
2020-04-18 19:19:01 +0530

    babel and direct separated

--- architects/webpack.architect.js
+++ architects/webpack.architect.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const {config} = require("../config/global.config")
 const _ = require("lodash");
 const {throwError} = require("../utils/cli-color");
+const readdir = require("recursive-dir-reader");
 
 /**
  * @return {{output: {}, entry: {}, plugins: [], module: {rules: []}}}
@@ -70,6 +71,12 @@ function buildReactConfig() {
 module.exports = (pages) => {
     return module.exports.withConfig(pages, getUserConfig())
 };
+module.exports.withConfig = (pages, config) => {
+    if (config.mode !== "production")
+        return module.exports.babel(pages, config);
+    else
+        return module.exports.direct(pages, config);
+};
 
 /**
  *
@@ -78,7 +85,9 @@ module.exports = (pages) => {
  * @param {String} conf webpack config
  * @return {[]}
  */
-module.exports.withConfig = (pages, conf) => {
+
+module.exports.babel = (pages, conf) => {
+    console.log("babel")
     if (!Array.isArray(pages))
         throwError("Expected array of pages got " + typeof pages);
     if (typeof conf !== "object")
@@ -90,6 +99,47 @@ module.exports.withConfig = (pages, conf) => {
         mode: config.mode,
         ...conf,
         //settings un-touchable by user
+    };
+    mergedConfig.output.path = mergedConfig.output.path || config.cache;
+    mergedConfig.output.filename = mergedConfig.output.filename || "[name].[hash].js"
+    mergedConfig.externals.React = "React";
+    mergedConfig.module.rules.push({
+        test: /\.js$/,
+        include: module.src,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ["@babel/preset-react"]
+            }
+        },
+    });
+    pages.forEach(page => {
+        let relative = page.replace(config.pages + "/", "");
+        let {dir, name} = path.parse(relative);
+        const entry = path.join(dir, name);
+        mergedConfig.target = 'node';
+        mergedConfig.entry[entry] = page;//create in one config
+        //make file as library so it can be imported for static generation
+        mergedConfig.output.libraryTarget = "commonjs2"
+    });
+
+    return [mergedConfig];
+}
+
+module.exports.direct = (pages, conf) => {
+    console.log("direct")
+    if (!Array.isArray(pages))
+        throwError("Expected array of pages got " + typeof pages);
+    if (typeof conf !== "object")
+        throwError(":( expected object got " + typeof conf);
+
+    let mergedConfig = {
+        //settings which can be changed by user
+        target: 'web',
+        mode: config.mode,
+        watch: config.mode === "development",
+        ...conf,
+        //settings un-touchable by user
         optimization: {
             splitChunks: {
                 chunks: 'all',
@@ -99,17 +149,11 @@ module.exports.withConfig = (pages, conf) => {
         },
     };
 
-    if (config.mode === "production") {
-        mergedConfig.watch = false;
-        mergedConfig.output.path = config.cache;
-    } else {
-        mergedConfig.watch = true;
-        mergedConfig.output.path = config.dist;
-        mergedConfig.externals.ReactDOM = "ReactDOM";
-    }
+    mergedConfig.output.path = config.dist;
     mergedConfig.output.filename = mergedConfig.output.filename || "[name].[hash].js"
 
     mergedConfig.externals.React = "React";
+    mergedConfig.externals.ReactDOM = "ReactDOM";
 
     mergedConfig.module.rules.push({
         test: /\.js$/,
@@ -127,31 +171,20 @@ module.exports.withConfig = (pages, conf) => {
         let relative = page.replace(config.pages + "/", "");
         let {dir, name} = path.parse(relative);
         const entry = path.join(dir, name);
-        if (mergedConfig.watch) {
-            outs.push(_.cloneDeep(mergedConfig));
-            outs[index].entry[entry] = path.resolve(__dirname, '../front/web-front.js');
-            outs[index].plugins.push(
-                new HtmlWebpackPlugin({
-                    filename: `${entry}.html`,
-                    template: path.resolve(__dirname, '../front/template.html'),
-                }),
-                new webpack.ProvidePlugin({
-                    App: "react",
-                })
-            );
-        } else {
-            mergedConfig.target = 'node';
-            mergedConfig.entry[entry] = page;//create in one config
-            //make file as library so it can be imported for static generation
-            mergedConfig.output.libraryTarget = "commonjs2"
-        }
+        outs.push(_.cloneDeep(mergedConfig));
+        outs[index].entry[entry] = path.resolve(__dirname, '../front/web-front.js');
+        outs[index].plugins.push(
+            new HtmlWebpackPlugin({
+                filename: `${entry}.html`,
+                template: path.resolve(__dirname, '../front/template.html'),
+            }),
+            new webpack.ProvidePlugin({
+                App: "react",
+            })
+        );
     });
-    if (!mergedConfig.watch) {//if in production
-        outs.push(mergedConfig);
-    }
     const libs = smartBuildLib();
     if (libs)
         outs.push(libs);
-    console.log(outs)
     return outs;
 }
--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 /*const {config} =require("./config/global.config");
 console.log(config);*/
-/*const {pageArchitect} = require("./index");
+const {pageArchitect} = require("./index");
 
-pageArchitect()*/
+pageArchitect()
 //require("./architects/plugin.architect")
 /*
 const indexPage = require("./dist/index.0f1704b357e5e167e785");
 console.log((indexPage))
 */
-const {renderToString} = require("react-dom/server");
+/*const {renderToString} = require("react-dom/server");
 global.React = require("react");
 const App = require("./out/.cache/index.a51c2eae9d502d912e5e");
-console.log(renderToString(React.createElement(App.default,{},null)));
+console.log(renderToString(React.createElement(App.default,{},null)));*/
 /*const index = require("./dist/index.59d6aff984e7b73585d6");
 index.default();*/
