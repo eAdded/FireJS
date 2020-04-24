3d1a4ba78aac79cfca5a59e847b38b0aa42184ed
Aniket Prajapati <prajapati.ani306@gmail.com>
2020-04-24 12:56:15 +0530

    pluginData.mapper.js done

--- architects/path.architect.js
+++ architects/path.architect.js
@@ -8,25 +8,8 @@ module.exports = class {
         this.#$ = globalData;
     }
 
-    build(page, paths, template) {
-        if (this.#$.map[page] === undefined) {//check if this page already exists
-            console.error(`page ${page} either does not exists or is not mapped. Hint : Make sure to add ${page} in map.`);
-            throw new Error();
-        } else {
-            try {
-                paths.forEach(path => {
-                    if (typeof path === "string") {
-                        console.log(StaticArchitect.createStatic(_path.join(this.#$.config.paths.babel, page), {name: "Aniket"}, template));
-                    }
-                });
-            } catch (e) {
-                if (!Array.isArray(paths)) {
-                    console.error(`Expected array got ${typeof paths}`)
-                    throw new Error();
-                } else
-                    throw e;
-            }
-        }
+    build(page, path,content , template) {
+        console.log(StaticArchitect.createStatic(_path.join(this.#$.config.paths.babel, page), content, template));
     }
 
 }
--- mappers/pluginData.mapper.js
+++ mappers/pluginData.mapper.js
@@ -14,8 +14,51 @@ module.exports = class {
         this.#$.config.plugins.forEach(plugin => {
             const plugData = require(plugin)();
             Object.keys(plugData).forEach(page => {
-                pathArchitect.build(page, plugData[page],template);
+                if (this.#$.map[page] === undefined) {//check if this page already exists
+                    console.error(`page ${page} either does not exists or is not mapped. Hint : Make sure to add ${page} in map.`);
+                    throw new Error();
+                } else {
+                    this.#$.map[page].didRender = true; // this helps in telling which paths have been already rendered
+                    const paths = plugData[page];
+                    if (Array.isArray(paths)) {
+                        paths.forEach(path => {
+                            if (typeof path === "string") {
+                                pathArchitect.build(page, path, {}, template);
+                            } else if (path.constructor.name === "AsyncFunction") {
+                                path().then(value => {
+                                    checkValidObject(value)
+                                    pathArchitect.build(page, value.path, value.content, template)
+                                });
+                            } else if (typeof path === "object") {
+                                checkValidObject(path);
+                                pathArchitect.build(page, path.path, path.content, template)
+                            }
+                        });
+                    } else {
+                        console.error(`Expected array got ${typeof paths}`)
+                        throw new Error();
+                    }
+                }
             });
         });
+        //render those pages which were not told by user
+        Object.keys(this.#$.map).forEach(page => {
+            if (!page.didRender) {
+                this.#$.map[page].didRender = true;
+                pathArchitect.build(page, page,{}, template);
+            }
+        })
+    }
+
+}
+
+function checkValidObject(value) {
+    if (typeof value.path !== "string") {
+        console.error(`Expected path:string got ${typeof value.path}`)
+        throw new Error();
+    }
+    if (typeof value.content !== "object") {
+        console.error(`Expected content:object got ${typeof value.path}`)
+        throw new Error();
     }
 }
