88414959893b164cd42744c1b51810ad564a39d8
Aniket Prajapati <prajapati.ani306@gmail.com>
2020-04-27 20:34:59 +0530

    copy only when pro

--- registrars/build.registrar.js
+++ registrars/build.registrar.js
@@ -10,11 +10,9 @@ module.exports = class {
     }
 
     autoRegister() {
-        if (this.#$.config.pro) {
-            const libRelative = _path.relative(this.#$.config.paths.dist, this.#$.config.paths.lib);
-            for (const mapComponent of this.#$.map.values())
-                this.registerComponentForSemiBuild(mapComponent, libRelative);
-        }
+        const libRelative = _path.relative(this.#$.config.paths.dist, this.#$.config.paths.lib);
+        for (const mapComponent of this.#$.map.values())
+            this.registerComponentForSemiBuild(mapComponent, libRelative);
     }
 
     registerComponentForSemiBuild(mapComponent, libRelative) {
@@ -22,16 +20,18 @@ module.exports = class {
         mapComponent.resolveOnSemiBuild(() => {
             mapComponent.chunks.forEach(chunk => {//copy chunks to lib
                 staticArchitect.addChunk(mapComponent, `${libRelative}/${chunk}`);
-                const absDir = _path.join(this.#$.config.paths.lib, mapComponent.getDir());
-                fs.mkdir(absDir, {recursive: true}, err => {
-                    if (err) {
-                        this.#$.cli.log("Error making dir " + absDir);
-                        throw err;
-                    }
-                    fs.rename(_path.join(this.#$.config.paths.babel, mapComponent.getDir(), chunk), _path.join(absDir, chunk), _ => {
-                        this.#$.cli.log("Moved chunk " + chunk);
+                if (this.#$.config.pro) {//only copy in production mode
+                    const absDir = _path.join(this.#$.config.paths.lib, mapComponent.getDir());
+                    fs.mkdir(absDir, {recursive: true}, err => {
+                        if (err) {
+                            this.#$.cli.log("Error making dir " + absDir);
+                            throw err;
+                        }
+                        fs.rename(_path.join(this.#$.config.paths.babel, mapComponent.getDir(), chunk), _path.join(absDir, chunk), _ => {
+                            this.#$.cli.log("Moved chunk " + chunk);
+                        });
                     });
-                });
+                }
             });
         });
     }
