bbed309c8bf19f5365ea34a83d72bcb8ad91ddc7
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-05-03 15:22:29 +0530

    browser history modification working

--- Components/Link.js
+++ Components/Link.js
@@ -2,7 +2,6 @@ export default ({to, children, className}) => {
     function load() {
         const map_script = document.createElement("script");
         map_script.src = `/${window.__MAP_REL__}${to === "/" ? "/index" : to}.map.js`;//make preloaded js to execute
-        console.log(map_script)
         document.head.appendChild(map_script);
         map_script.onload = () => {
             window.__MAP__.chunks.forEach(chunk => {
@@ -10,7 +9,6 @@ export default ({to, children, className}) => {
                 preloadLink.href = `/${window.__LIB_REL__}/` + chunk;
                 preloadLink.rel = "preload";
                 preloadLink.as = "script";//this preloads script before hand
-                console.log(preloadLink)
                 document.head.appendChild(preloadLink);
             })
         };
@@ -20,13 +18,13 @@ export default ({to, children, className}) => {
         window.__MAP__.chunks.forEach(chunk => {
             const preloadedScript = document.createElement("script");
             preloadedScript.src = `/${window.__LIB_REL__}/` + chunk;//make preloaded js to execute
-            console.log(preloadedScript)
             document.head.appendChild(preloadedScript);
         });
+        window.history.pushState('', '', to);
     }
 
     return (
-        <a className={className} onClick={apply} onMouseEnter={load}>
+        <a href="javascript:void(0)" className={className} onClick={apply} onMouseEnter={load}>
             {children}
         </a>
     )
