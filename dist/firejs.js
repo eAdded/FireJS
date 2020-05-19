9d9811252a79ca01906bc550a322d72b2a8da10f
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-05-19 18:22:05 +0530

    gen babel chunk map

--- dist/firejs.js
+++ dist/firejs.js
@@ -3,10 +3,20 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./index");
 const server_1 = require("./server");
+const fs_1 = require("fs");
+const path_1 = require("path");
 const app = new index_1.default();
 if (app.Context.config.pro)
     app.buildPro(() => {
-        app.Context.cli.ok("DONE ⌐■-■");
+        const $ = app.Context;
+        $.cli.ok("Build finished");
+        $.cli.log("Generating babel chunk map");
+        const babel_map = {};
+        for (const mapComponent of $.map.values()) {
+            babel_map[mapComponent.Page] = mapComponent.babelChunk;
+        }
+        fs_1.writeFileSync(path_1.join($.config.paths.out, "babel.map.json"), JSON.stringify(babel_map));
+        $.cli.ok("DONE");
     });
 else {
     new server_1.default(app);
--- src/firejs.ts
+++ src/firejs.ts
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 import FireJS from "./index"
 import Server from "./server"
+import {writeFileSync} from "fs"
+import {join} from "path"
 
 const app = new FireJS();
 if (app.Context.config.pro)
     app.buildPro(() => {
-        app.Context.cli.ok("DONE ⌐■-■")
+        const $ = app.Context;
+        $.cli.ok("Build finished");
+        $.cli.log("Generating babel chunk map");
+        const babel_map = {};
+        for (const mapComponent of $.map.values()) {
+            babel_map[mapComponent.Page] = mapComponent.babelChunk;
+        }
+        writeFileSync(join($.config.paths.out, "babel.map.json"), JSON.stringify(babel_map));
+        $.cli.ok("DONE");
     });
 else {
     new Server(app);
