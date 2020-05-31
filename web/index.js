3e7549504808c6716719fb46ee26499f7573c742
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-05-31 12:24:08 +0530

    conditional render and hydrate

--- web/index.js
+++ web/index.js
@@ -1,5 +1,10 @@
 import Wrapper from "./Wrapper";
 
-ReactDOM.hydrate(<Wrapper content={window.__MAP__.content}/>,
-    document.getElementById("root")
-);
+if (window.__SSR__)
+    ReactDOM.hydrate(<Wrapper content={window.__MAP__.content}/>,
+        document.getElementById("root")
+    );
+else
+    ReactDOM.render(<Wrapper content={window.__MAP__.content}/>,
+        document.getElementById("root")
+    );
