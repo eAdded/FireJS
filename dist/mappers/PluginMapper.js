8cf6d0f2ec7da2dad4a390b98ace55825e9525f8
aniketfuryrocks <prajapati.ani306@gmail.com>
2020-06-08 21:33:17 +0530

    new Plugin

--- dist/mappers/PluginMapper.js
+++ dist/mappers/PluginMapper.js
@@ -1,18 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Fs_1 = require("../utils/Fs");
+const path_1 = require("path");
 function mapPlugins(inputFileSystem, pluginsPath, map) {
-    Fs_1.readDirRecursively(pluginsPath, inputFileSystem, pluginFile => {
-        const plugin = new (require(pluginFile).default)();
-        // @ts-ignore
-        if ((plugin.version || "") < global.__MIN_PLUGIN_VERSION__)
+    inputFileSystem.readdirSync(pluginsPath).forEach(pluginFile => {
+        const plugins = require(path_1.join(pluginsPath, pluginFile));
+        for (const name in plugins) {
+            const plugin = new plugins[name](name);
             // @ts-ignore
-            throw new Error(`Plugin ${pluginFile} is not supported. Update plugin to v` + global.__MIN_PLUGIN_VERSION__);
-        const page = map.get(plugin.page);
-        if (page)
-            page.plugin = plugin;
-        else
-            throw new Error(`Page ${plugin.page} requested by plugin ${pluginFile} does not exist`);
+            if ((plugin.version || "") < global.__MIN_PLUGIN_VERSION__)
+                // @ts-ignore
+                throw new Error(`Plugin ${pluginFile} is not supported. Update plugin to v` + global.__MIN_PLUGIN_VERSION__);
+            const page = map.get(name);
+            if (page)
+                page.plugin = plugin;
+            else
+                throw new Error(`Page ${plugin.page} requested by plugin ${pluginFile} does not exist`);
+        }
     });
 }
 exports.mapPlugins = mapPlugins;
--- src/mappers/PluginMapper.ts
+++ src/mappers/PluginMapper.ts
@@ -1,20 +1,21 @@
 import {join} from "path";
 import Plugin from "../classes/Plugin";
 import Page from "../classes/Page";
-import {readDirRecursively} from "../utils/Fs";
 
 export function mapPlugins(inputFileSystem, pluginsPath: string, map: Map<string, Page>) {
-    readDirRecursively(pluginsPath, inputFileSystem, pluginFile => {
-        const plugin: Plugin = new (require(pluginFile).default)();
-        // @ts-ignore
-        if ((plugin.version || "") < global.__MIN_PLUGIN_VERSION__)
+    inputFileSystem.readdirSync(pluginsPath).forEach(pluginFile => {
+        const plugins = require(join(pluginsPath, pluginFile));
+        for (const name in plugins) {
+            const plugin: Plugin = new plugins[name](name);
             // @ts-ignore
-            throw new Error(`Plugin ${pluginFile} is not supported. Update plugin to v` + global.__MIN_PLUGIN_VERSION__);
-
-        const page = map.get(plugin.page);
-        if (page)
-            page.plugin = plugin;
-        else
-            throw new Error(`Page ${plugin.page} requested by plugin ${pluginFile} does not exist`)
+            if ((plugin.version || "") < global.__MIN_PLUGIN_VERSION__)
+                // @ts-ignore
+                throw new Error(`Plugin ${pluginFile} is not supported. Update plugin to v` + global.__MIN_PLUGIN_VERSION__);
+            const page = map.get(name);
+            if (page)
+                page.plugin = plugin;
+            else
+                throw new Error(`Page ${plugin.page} requested by plugin ${pluginFile} does not exist`)
+        }
     });
 }
