b3d5ba7d19d776ef16a891b4c4da8218cd238ef0
Aniket Prajapati <prajapati.ani306@gmail.com>
2020-06-17 11:48:09 +0530

    Made FireJS_Require.js async

--- web/FireJS_Require.js
+++ web/FireJS_Require.js
@@ -1,12 +1,13 @@
 (() => {
     window.__COUNT__ = 0;
     let done = 0;
-    window.FireJS_Require = async function (chunkPromise, id, options, children) {
+    window.FireJS_Require = function (chunkPromise, id, options, children) {
         window.__COUNT__++;
-        const chunk = await chunkPromise;
-        (window.__HYDRATE__ ? ReactDOM.hydrate : ReactDOM.render)
-        (React.createElement(chunk.default, options, children), document.getElementById(id));
-        if (window.__SSR__ && ++done === window.__COUNT__)
-            window.finish();
+        chunkPromise.then(chunk => {
+            (window.__HYDRATE__ ? ReactDOM.hydrate : ReactDOM.render)
+            (React.createElement(chunk.default, options, children), document.getElementById(id));
+            if (window.__SSR__ && ++done === window.__COUNT__ )
+                window.finish();
+        })
     }
 })()
