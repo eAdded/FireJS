3b336a0696c541da97331a4265f7a791f92c1a57
Aniket Prajapati <prajapati.ani306@gmail.com>
2020-06-18 22:45:58 +0530

    working

--- comps/LazyLoad.js
+++ comps/LazyLoad.js
@@ -1,13 +1,16 @@
-export default (chunk, {ssr, script, delay = 200, placeHolder = ""}) => {
+FireJS.lazyCount = 0;
+FireJS.lazyDone = 0;
+export default (chunk, {ssr = true, script, delay = 200, placeHolder = ""} = {}) => {
+    FireJS.lazyCount++;
     if (script && ssr)
         throw new Error("Scripts can't be rendered. Set either script or ssr to false");
     if (FireJS.isSSR)
         delay = 0;
     const id = `FireJS_LAZY_${FireJS.lazyCount}`;
-    FireJS.lazyCount++;
     let compProps = {};
     chunk.then(chunk =>
         setTimeout(() => {
+            FireJS.lazyDone++;
             if (!script) {
                 let func;
                 if (FireJS.isSSR)
@@ -21,16 +24,17 @@ export default (chunk, {ssr, script, delay = 200, placeHolder = ""}) => {
                     func = ReactDOM.render;
                 func(React.createElement(chunk.default, compProps, compProps.children), document.getElementById(id))
             }
-            if ((++FireJS.lazyDone === FireJS.lazyCount) && FireJS.isSSR)
+            if (FireJS.lazyDone === FireJS.lazyCount && FireJS.isSSR)
                 FireJS.finishRender();
         }, delay)
     )
-    return function (props) {
-        compProps = props;
-        return (
-            <div id={id}>
-                {placeHolder}
-            </div>
-        )
-    }
+    if (!script)
+        return function (props) {
+            compProps = props;
+            return (
+                <div id={id}>
+                    {placeHolder}
+                </div>
+            )
+        }
 }
