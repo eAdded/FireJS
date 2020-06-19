5b876decbc8874f70d43a584d6fead281121da04
Aniket Prajapati <prajapati.ani306@gmail.com>
2020-06-19 16:02:56 +0530

    Lazy load

--- code/src/components/LazyLoad.js
+++ code/src/components/LazyLoad.js
@@ -1,6 +1,6 @@
 FireJS.lazyCount = 0;
 FireJS.lazyDone = 0;
-export default (chunk, {ssr = true, script, delay = 200, placeHolder = ""} = {}) => {
+export default (chunk, {ssr = true, script, delay = 0, placeHolder = ""} = {}) => {
     FireJS.lazyCount++;
     if (script && ssr)
         throw new Error("Scripts can't be rendered. Set either script or ssr to false");
@@ -18,10 +18,8 @@ export default (chunk, {ssr = true, script, delay = 200, placeHolder = ""} = {})
                         func = ReactDOM.render;
                     else
                         return;
-                else if (ssr)
-                    func = ReactDOM.hydrate
                 else
-                    func = ReactDOM.render;
+                    func = FireJS.isHydrated ? ReactDOM.hydrate : ReactDOM.render;
                 func(React.createElement(chunk.default, compProps, compProps.children), document.getElementById(id))
             }
             if (FireJS.lazyDone === FireJS.lazyCount && FireJS.isSSR)
